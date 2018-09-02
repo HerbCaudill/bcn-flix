5586579a399cda14c1edc0c12e60e3e764b03754
Herb Caudill <herb@caudillweb.com>
2018-09-02 17:38:52 +0200

    Add Apollo client.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
 import * as React from 'react';
+import {ApolloProvider, Query} from 'react-apollo';
+import gql from 'graphql-tag';
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "/graphql"
+});
+
 import './App.css';
 
 import logo from './logo.svg';
 
+
+const Books = () => (
+  <Query query={gql`
+    {
+      books {
+        title
+        author
+      }
+    }
+  `}>
+   {({ loading, error, data}) => {
+     if (loading) return <p>Loading...</p>;
+     if (error) return <p>Error</p>;
+
+     return data.books.map(({title, author}: any) => (
+       <div key={title}>
+          <p>{title} ({author})</p>
+       </div>
+     ))
+
+   }}
+  </Query>
+)
+
+
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <h2>Books</h2>
+          <Books></Books>
+        </div>
+      </ApolloProvider>
     );
   }
 }
