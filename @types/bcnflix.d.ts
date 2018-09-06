813f4ddb5ee862314babb4adf4297b427fce3bc1
Herb Caudill <herb@caudillweb.com>
2018-09-06 16:44:37 +0200

    Add types for this app: `Movie`, `Theater`, `Showtimes`

---
+++ @types/bcnflix.d.ts
@@ -0,0 +1,34 @@
+import * as Joda from 'js-joda'
+
+declare namespace bcnflix {
+  type Movie = {
+    id?: string
+    localTitle: string
+    title?: string
+    poster?: string
+    localPoster?: string
+    description?: string
+    localDescription?: string
+    releaseDate?: Joda.LocalDate
+    language?: string
+    productionCountries?: string[]
+    spokenLanguages?: string[]
+    runtime?: Joda.Duration
+    genres?: string[]
+    trailerLink?: string
+    localRating?: number
+    metascore?: number
+    compositeScore?: number
+    showtimes?: Showtimes[]
+  }
+
+  type Theater = {
+    id?: string
+    name?: string
+  }
+
+  type Showtimes = {
+    theater?: Theater
+    times?: Joda.LocalTime[]
+  }
+}
