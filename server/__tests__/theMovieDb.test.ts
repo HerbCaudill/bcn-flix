9be457701a42091173230102d33bb8697e291c76
Herb Caudill <herb@caudillweb.com>
2018-09-13 11:09:14 +0200

    tmdb tests

---
+++ server/__tests__/theMovieDb.test.ts
@@ -0,0 +1,25 @@
+import * as Joda from 'js-joda'
+
+jest.mock('theMovieDb-javascript-library')
+
+import lookupMovie from '../src/theMovieDb'
+
+it('looks up a movie', async () => {
+  const movie = await lookupMovie('Campeones')
+
+  const t = (key: string, value: any) =>
+    expect(movie).toEqual(expect.objectContaining({ [key]: value }))
+    
+  t('description', expect.stringContaining('A disgraced basketball coach'))
+  t('genres', [{ id: 18, name: 'Drama' }, { id: 35, name: 'Comedy' }])
+  t('id', 456929)
+  t('language', 'es')
+  t('localTitle', 'Campeones')
+  t('poster', expect.stringContaining('m5z4Ud6Ya5EY3Eg3OBbVBaDKWK.jpg'))
+  t('productionCountries', [{ iso_3166_1: 'ES', name: 'Spain' }])
+  t('releaseDate', Joda.LocalDate.parse('2018-04-06'))
+  t('runtime', Joda.Duration.ofMinutes(124))
+  t('spokenLanguages', [{ iso_639_1: 'es', name: 'Español' }])
+  t('title', 'Champions')
+  t('tmdbRating', 7.8)
+})
