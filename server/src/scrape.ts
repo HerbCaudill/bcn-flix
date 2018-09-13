e082a6f968a9ec8ecfbd4db6208c37c81d17bc4a
Herb Caudill <herb@caudillweb.com>
2018-09-13 14:34:18 +0200

    encapsulate scrape

---
+++ server/src/scrape.ts
@@ -0,0 +1,30 @@
+import request from 'request-promise-cache'
+import PromiseThrottle from 'promise-throttle'
+
+const IMA_BROWSER =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+
+const throttle = new PromiseThrottle({
+  requestsPerSecond: 3,
+  promiseImplementation: Promise,
+})
+
+const scrape = async (url: string): Promise<string> => {
+  const pageRequest = (url: string) =>
+    request({
+      url,
+      headers: { 'User-Agent': IMA_BROWSER },
+      cacheKey: url,
+      cacheTTL: 60 * 60 * 1000,
+      limit: 0,
+    })
+
+  const html = await throttle
+    .add(pageRequest.bind(this, url))
+    .catch((err: Error) => {
+      return ''
+    })
+  return html
+}
+
+export default scrape
