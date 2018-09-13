dd3a58b578c78e3586fb703d87ea15f091600a7c
Herb Caudill <herb@caudillweb.com>
2018-09-13 11:08:08 +0200

    mock tmdb

---
+++ server/__mocks__/themoviedb-javascript-library.ts
@@ -0,0 +1,27 @@
+import { read } from '../__tests__/_utils/assets'
+
+export default {
+  common: {
+    api_key: '',
+  },
+  search: {
+    getMovie(
+      { query }: { query: string },
+      success: (result: any) => void,
+      failure: (result: any) => void
+    ) {
+      const result = read.theMovieDb.search(query)
+      success(result)
+    },
+  },
+  movies: {
+    getById(
+      { id }: { id: string },
+      success: (result: any) => void,
+      failure: (result: any) => void
+    ) {
+      const result = read.theMovieDb.findById(id)
+      success(result)
+    },
+  },
+}
--- server/__tests__/_utils/assets.ts
+++ server/__tests__/_utils/assets.ts
@@ -1,10 +1,31 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
+// Sanitize windows filename
+const sanitize = (s: string): string =>
+  s.replace('^\\.+', '').replace('[\\\\/:*?"<>|]', '')
+
 export const readModuleFile = (modulePath: string) => {
   const filename = path.join(__dirname, modulePath)
   return fs.readFileSync(filename, 'utf8')
 }
 
-export const readAsset = (path: string): string =>
-  readModuleFile(`../_assets/${path}`)
+export const read = {
+  asset(path: string): string {
+    return readModuleFile(`../_assets/${path}`)
+  },
+
+  sensacine(id: string): string {
+    return read.asset(`sensacine/${id}.html`)
+  },
+
+  theMovieDb: {
+    search(query: string): any {
+      const safeQuery = sanitize(query)
+      return JSON.parse(read.asset(`theMovieDb/search/${safeQuery}.json`))
+    },
+    findById(id: string): any {
+      return JSON.parse(read.asset(`theMovieDb/movie/${id}.json`))
+    },
+  },
+}
