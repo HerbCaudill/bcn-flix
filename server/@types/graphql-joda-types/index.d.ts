61f3a361d330ba897e352d2356ac0f3cb4fa1c25
Herb Caudill <herb@caudillweb.com>
2018-09-14 10:42:19 +0200

    more folder + index.d.ts

---
+++ server/@types/graphql-joda-types/index.d.ts
@@ -0,0 +1,62 @@
+declare module 'graphql-joda-types' {
+  export const LocalDate: {
+    astNode: any
+    description: string
+    extensionASTNodes: any
+    inspect: any
+    name: string
+    parseLiteral: any
+    parseValue: any
+    serialize: any
+    toJSON: any
+    toString: any
+  }
+  export const LocalDateTime: {
+    astNode: any
+    description: string
+    extensionASTNodes: any
+    inspect: any
+    name: string
+    parseLiteral: any
+    parseValue: any
+    serialize: any
+    toJSON: any
+    toString: any
+  }
+  export const LocalTime: {
+    astNode: any
+    description: string
+    extensionASTNodes: any
+    inspect: any
+    name: string
+    parseLiteral: any
+    parseValue: any
+    serialize: any
+    toJSON: any
+    toString: any
+  }
+  export const ZoneId: {
+    astNode: any
+    description: string
+    extensionASTNodes: any
+    inspect: any
+    name: string
+    parseLiteral: any
+    parseValue: any
+    serialize: any
+    toJSON: any
+    toString: any
+  }
+  export const ZonedDateTime: {
+    astNode: any
+    description: string
+    extensionASTNodes: any
+    inspect: any
+    name: string
+    parseLiteral: any
+    parseValue: any
+    serialize: any
+    toJSON: any
+    toString: any
+  }
+}
