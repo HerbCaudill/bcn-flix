560bf5c5451a0d7f663fce4c4a4c89f37a3353d7
Herb Caudill <herb@caudillweb.com>
2018-09-20 15:24:34 +0200

    filter movies (exclude hidden)

--- client/src/components/Index.tsx
+++ client/src/components/Index.tsx
@@ -1,66 +1,80 @@
 import * as React from 'react'
 import Movie from './Movie'
+import Toolbar from './Toolbar'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import { MovieInfo } from '../../../server/@types/bcnflix'
 
+const MOVIES_QUERY = gql`
+  query movies {
+    exclusions @client
+    movies {
+      id
+      title
+      alternateTitle
+      poster
+      localPoster
+      description
+      localDescription
+      releaseDate
+      language
+      countries
+      languages
+      runtime
+      genres
+      trailerLink
+      localRating
+      tmdbRating
+      metascore
+      compositeScore
+      showtimes {
+        theater {
+          id
+          name
+        }
+        times
+      }
+    }
+  }
+`
+const LOADING_MESSAGE = <p>Loading...</p>
+
+const errorMessage = ({ message }: { message: string }) => (
+  <div className="ui message">
+    <div className="header">GraphQL error</div>
+    <pre>{message}</pre>
+    <p>
+      {message == 'Network error: Failed to fetch' &&
+        'Is the API server running?'}
+    </p>
+  </div>
+)
+
+const descendingByCompositeScore = (a: MovieInfo, b: MovieInfo) =>
+  (b.compositeScore || 0) - (a.compositeScore || 0)
+
 const Index = () => {
   return (
-    <Query
-      query={gql`
-        {
-          movies {
-            title
-            alternateTitle
-            poster
-            localPoster
-            description
-            localDescription
-            releaseDate
-            language
-            countries
-            languages
-            runtime
-            genres
-            trailerLink
-            localRating
-            tmdbRating
-            metascore
-            compositeScore
-            showtimes {
-              theater {
-                id
-                name
-              }
-              times
-            }
-          }
-        }
-      `}
-    >
+    <Query query={MOVIES_QUERY}>
       {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>
-        if (error)
-          return (
-            <div className="ui message">
-              <div className="header">GraphQL error</div>
-              <pre>{error.message}</pre>
-              <p>
-                {error.message == 'Network error: Failed to fetch' &&
-                  'Is the API server running?'}
-              </p>
-            </div>
-          )
-        // Sort by composite score, descending
-        data.movies.sort(
-          (a: MovieInfo, b: MovieInfo) =>
-            (b.compositeScore || 0) - (a.compositeScore || 0)
+        if (loading) return LOADING_MESSAGE
+        if (error) return errorMessage(error)
+        if (!data.movies || data.movies.length == 0)
+          return errorMessage({ message: 'No movies found' })
+
+        console.log(data.exclusions)
+        data.movies.sort(descendingByCompositeScore)
+        const filteredMovies = data.movies.filter(
+          (d: MovieInfo) => !data.exclusions.includes(d.id)
         )
         return (
-          <div className="ui cards">
-            {data.movies.map((d: MovieInfo) => (
-              <Movie info={d} key={d.title} />
-            ))}
+          <div>
+            <Toolbar />
+            <div className="ui cards">
+              {filteredMovies.map((d: MovieInfo) => (
+                <Movie info={d} key={d.title} />
+              ))}
+            </div>
           </div>
         )
       }}
